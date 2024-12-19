d641fbdea891c96c2dfaa43cfb788acbcdcdc95f
calegario <calegario.souza@outlook.com>
2024-12-18 23:12:58 -0300

    add play2.js

--- cypress/e2e/play2_automation_camp.cy.js
+++ cypress/e2e/play2_automation_camp.cy.js
@@ -2,25 +2,25 @@ import { homePlay2 } from "../support/play2"
 describe('template spec', () => {
   it('click buttons', () => {
     cy.visit('https://play2.automationcamp.ir/')
-    cy.contains('Your Sample Alert Button!').click()
-    cy.contains('You Pressed the OK Button!')
-    cy.contains('Double-click me').dblclick();
+    cy.contains(homePlay2.buttons.alertButton).click()
+    cy.contains(homePlay2.labels.youPressedButton)
+    cy.contains(homePlay2.buttons.doubleClick).dblclick();
 
-    cy.get('#uname').type('test')
-    cy.get('#pwd').type('test')
+    cy.get(homePlay2.Inputs.userLogin).type(homePlay2.values.login)
+    cy.get(homePlay2.Inputs.passwordLogin).type(homePlay2.values.password)
 
-    cy.get('input[type="Submit"]').contains('Login').click()
+    cy.get(homePlay2.buttons.loginButton).contains(homePlay2.labels.loginText).click()
 
-    cy.contains('Login Successful :)')
-    cy.contains('Click here to go back to the home page')
-    cy.contains('here').click()
+    cy.contains(homePlay2.labels.succsessLogin)
+    cy.contains(homePlay2.labels.back)
+    cy.contains(homePlay2.buttons.clickHere).click()
     
-    cy.get('#fname').type('João Victor')
-    cy.get('#lname').type('Calegário')
-    cy.contains('Male').click()
+    cy.get(homePlay2.Inputs.firstName).type('João Victor')
+    cy.get(homePlay2.Inputs.lastName).type('Calegário')
+    cy.contains(homePlay2.radioButton.genderMale).click()
 
-    cy.get('#option').select('Option 3')
-    cy.get('input[type="checkbox"]').check(['Option 1', 'Option 3'])
+    cy.get(homePlay2.radioButton.option).select(homePlay2.radioButton.option3Text)
+    cy.get().check(['Option 1', 'Option 3'])
     cy.get('input[name="Options"]').type('Chocolate')
 
     cy.get('#favcolor').click()
--- cypress/support/play2.js
+++ cypress/support/play2.js
@@ -1,10 +1,37 @@
-const homePlay2 ={
-    buttons:{
-
+const homePlay2 = {
+    buttons: {
+        doubleClick: 'Double-click me',
+        alertButton: 'Your Sample Alert Button!',
+        loginButton: "input[type='Submit']",
+        clickHere: 'here',
     },
     Inputs: {
+        userLogin: '#uname',
+        passwordLogin: '#pwd',
+        firstName: 'fname',
+        lastName: '#lname'
 
+    },
+    values: {
+        login:'test',
+        password: 'test',
+        option1: 'Option 1',
+        option3: 'Option 3',
+        genderMale: 'Male',
+
+    },
+    labels: {
+        youPressedButton: 'You Pressed the OK Button!',
+        loginText: "Login",
+        succsessLogin: 'Login Successful :)',
+        back: 'Click here to go back to the home page'
+    },
+    radioButton: {
+        option: '#option',
+    },
+    checkBox: {
+        option: 'input[type="checkbox"]',
     }
 };
 
-export { homePlay2 }
+export { homePlay2 }    
